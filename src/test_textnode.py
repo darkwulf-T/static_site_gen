250a1cdc839c54d3c47346707a17899b2eaff4b4
darkwulf <tech.darkwulf@gmail.com>
2025-10-04 00:07:55 +0200

    Chapter 3.1 finished

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimeter
 
 
 class TestTextNode(unittest.TestCase):
@@ -46,6 +46,36 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This is a text node")
         self.assertEqual(html_node.props, {"href": "https://www.google.com"})
+
+    def test_split_text(self):
+        node1 = TextNode("This is text with a `code block` word", TextType.PLAIN_TEXT)
+        node2 = TextNode("This is a **text node**", TextType.PLAIN_TEXT)
+        old_nodes = [node1, node2]
+        result = [
+            TextNode("This is text with a ", TextType.PLAIN_TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.PLAIN_TEXT),
+            TextNode("This is a **text node**", TextType.PLAIN_TEXT)
+            ]
+        self.assertEqual(split_nodes_delimeter(old_nodes, "`", TextType.CODE), result)
+
+    def test_split_text2(self):
+        node1 = TextNode("This is text with a `code block` word", TextType.PLAIN_TEXT)
+        node2 = TextNode("This is a **text node**", TextType.PLAIN_TEXT)
+        old_nodes = [node1, node2]
+        result = [
+            TextNode("This is text with a `code block` word", TextType.PLAIN_TEXT),
+            TextNode("This is a ", TextType.PLAIN_TEXT),
+            TextNode("text node", TextType.BOLD)
+            ]
+        self.assertEqual(split_nodes_delimeter(old_nodes, "**", TextType.BOLD), result)
+
+    def test_split_text3(self):
+        node1 = TextNode("This is text with a `code block` word", TextType.PLAIN_TEXT)
+        node2 = TextNode("This is a **text node", TextType.PLAIN_TEXT)
+        old_nodes = [node1, node2]
+        with self.assertRaisesRegex(ValueError, "invalid markdown, formatted section not closed"):
+            split_nodes_delimeter(old_nodes, "**", TextType.BOLD)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -38,3 +38,23 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    
+def split_nodes_delimeter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for nodes in old_nodes:
+        if nodes.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(nodes)
+            continue
+        split_nodes = []
+        sections = nodes.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.PLAIN_TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
